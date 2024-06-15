77387baa6c472b989be77335cf9edce31c17fd35
fsdevandrii <135020138+fsdevandrii@users.noreply.github.com>
2024-06-15 22:37:18 +0300

    init

--- lib/utils/api-routes.ts
+++ lib/utils/api-routes.ts
@@ -1,4 +1,5 @@
-import { MongoClient } from 'mongodb'
+import { Db, MongoClient } from 'mongodb'
+import { shuffle } from './common'
 
 export const getDbAndReqBody = async (
   clientPromise: Promise<MongoClient>,
@@ -12,4 +13,21 @@ export const getDbAndReqBody = async (
   }
 
   return { db }
+}
+
+export const getNewAndBestsellerGoods = async (db: Db, fieldName: string) => {
+  const clothes = await db.collection('cloth').find().toArray()
+  const accessories = await db.collection('accessories').find().toArray()
+
+  return shuffle([
+    ...clothes
+      .filter(
+        (item) =>
+          item[fieldName] && Object.values(item.sizes).some((value) => value)
+      )
+      .slice(0, 2),
+    ...accessories
+      .filter((item) => item[fieldName] && !Object.values(item.sizes).length)
+      .slice(0, 2),
+  ])
 }
